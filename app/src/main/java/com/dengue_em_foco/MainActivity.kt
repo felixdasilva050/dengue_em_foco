15371837af8594d211251f6279bf7a2bb6c44e76
Guilherme Silva <guilhermefelix.ofwork@gmail.com>
2024-06-10 02:18:00 -0300

    refactor: get saved history

--- app/src/main/java/com/dengue_em_foco/MainActivity.kt
+++ app/src/main/java/com/dengue_em_foco/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
 
         val loginButton = findViewById<Button>(R.id.main_login_button)
         loginButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, DengueNoticesActivity::class.java)
             startActivity(intent)
         }
 
